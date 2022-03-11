4c87c05b0b8ef5f83e4bf5905f72ccd33e062771
zq99299 <99299684@qq.com>
2022-03-11 19:43:49 +0800

    均衡负载策略

--- ribbon/ribbon-consumer/src/main/java/cn/mrcode/springcloud/AppConfig.java
+++ ribbon/ribbon-consumer/src/main/java/cn/mrcode/springcloud/AppConfig.java
@@ -1,7 +1,9 @@
 package cn.mrcode.springcloud;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
  * @date 2022/3/7 21:53
  */
 @Configuration
+@RibbonClient(name = "eureka-client", configuration = RandomRule.class)
 public class AppConfig {
     @Bean
     // 用于标记要配置为使用 LoadBalancerClient 的 RestTemplate 或 WebClient bean 的注解
@@ -18,4 +21,9 @@ public class AppConfig {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+//    @Bean
+//    public IRule defaultLBStrategy() {
+//        return new RandomRule();
+//    }
 }
